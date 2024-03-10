f0911a7d29da462bcbdaef683f00c12a7a60a3ab
dervovan <dervovan@gmail.com>
2024-03-10 18:38:46 +0300

    handle ripple row function no longer "clean" - for better performance

--- src/components/gol/rippleLogic.ts
+++ src/components/gol/rippleLogic.ts
@@ -17,9 +17,6 @@ type rowHandleParams = {
 
 type handleRowResult = {
   isLastProcessed: boolean;
-  topRow?: Array<CellState>;
-  currentRow: Array<CellState>;
-  bottomRow?: Array<CellState>;
 };
 
 export const waterRipple = (
@@ -27,21 +24,16 @@ export const waterRipple = (
   rippleCenter: rippleCenter
 ) => {
   const newBoard = initRippleState(board, rippleCenter);
-  const copy = JSON.parse(JSON.stringify(newBoard));
-  for (let i = 0; i < copy.length; i++) {
-    const result = handleRow({
+  for (let i = 0; i < newBoard.length; i++) {
+    const isLastProcessed = handleRow({
       currentIndex: i,
-      topRow: copy[i - 1],
-      currentRow: copy[i],
-      bottomRow: copy[i + 1],
+      topRow: newBoard[i - 1],
+      currentRow: newBoard[i],
+      bottomRow: newBoard[i + 1],
       rippleCenter,
     });
 
-    newBoard[i] = result.currentRow;
-    if (result.topRow) newBoard[i - 1] = result.topRow;
-    if (result.bottomRow) newBoard[i + 1] = result.bottomRow;
-
-    if (result.isLastProcessed) break;
+    if (isLastProcessed) break;
   }
 
   return newBoard;
@@ -53,46 +45,36 @@ const handleRow = ({
   topRow,
   bottomRow,
   rippleCenter: { cellIndex, rowIndex },
-}: rowHandleParams): handleRowResult => {
-  const result: handleRowResult = {
-    currentRow: [...currentRow],
-    isLastProcessed: false,
-  };
-
+}: rowHandleParams): boolean => {
   for (let i = 0; i < currentRow.length; i++) {
     if (!currentRow[i]) continue;
-    result.currentRow[i] = 0;
+    currentRow[i] = 0;
     if (i > cellIndex) {
-      safeUpdate(result.currentRow, i + 1, 1);
-      break;
+      safeUpdate(currentRow, i + 1, 1);
+      return false;
     } else if (i < cellIndex) {
-      result.currentRow[i - 1] = 1;
+      safeUpdate(currentRow, i - 1, 1);
     } else {
-      safeUpdate(result.currentRow, i - 1, 1);
-      safeUpdate(result.currentRow, i + 1, 1);
+      safeUpdate(currentRow, i - 1, 1);
+      safeUpdate(currentRow, i + 1, 1);
       if (currentIndex < rowIndex && topRow) {
-        result.topRow = [...topRow];
-        result.topRow[i] = 1;
+        topRow[i] = 1;
       } else if (currentIndex > rowIndex && bottomRow) {
-        result.bottomRow = [...bottomRow];
-        result.bottomRow[i] = 1;
-        result.isLastProcessed = true;
+        bottomRow[i] = 1;
+        return true;
       } else if (currentIndex === rowIndex) {
         if (bottomRow) {
-          result.bottomRow = [...bottomRow];
-          result.bottomRow[i] = 1;
+          bottomRow[i] = 1;
         }
         if (topRow) {
-          result.topRow = [...topRow];
-          result.topRow[i] = 1;
+          topRow[i] = 1;
         }
-        result.isLastProcessed = true;
+        return true;
       }
       break;
     }
   }
-
-  return result;
+  return false;
 };
 
 const initRippleState = (
@@ -114,19 +96,5 @@ const initRippleState = (
   );
 
   newBoard[rowIndex][cellIndex] = 1;
-
-  // if (newBoard[rowIndex - 1] !== undefined) {
-  //   newBoard[rowIndex - 1][cellIndex] = 1;
-  // }
-  // if (newBoard[rowIndex]?.[cellIndex - 1] !== undefined) {
-  //   newBoard[rowIndex][cellIndex - 1] = 1;
-  // }
-  // if (newBoard[rowIndex + 1] !== undefined) {
-  //   newBoard[rowIndex + 1][cellIndex] = 1;
-  // }
-  // if (newBoard[rowIndex]?.[cellIndex + 1] !== undefined) {
-  //   newBoard[rowIndex][cellIndex + 1] = 1;
-  // }
-
   return newBoard;
 };
